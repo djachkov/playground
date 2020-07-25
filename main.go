803d24accd3afe979c65424b6263f1be46c1aa17
Dmitriy Diachkov <dmitriy.diachkov@itiviti.com>
2020-07-25 13:06:42 +0300

    First code drafts

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+	"net/http/httputil"
+)
+
+func hello(w http.ResponseWriter, request *http.Request) {
+	tmpl := template.Must(template.ParseFiles("index.html"))
+	tmpl.Execute(w, "")
+}
+func world(w http.ResponseWriter, request *http.Request) {
+	dumpedRequest, err := httputil.DumpRequest(request, true)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Fprintf(w, string(dumpedRequest))
+}
+func main() {
+
+	http.HandleFunc("/hello", hello)
+	http.HandleFunc("/world", world)
+	http.ListenAndServe(":8090", nil)
+}
